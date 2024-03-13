77d1c7601c7d30e856eb43c1762f0d4f646d36c6
justin-tanvt <justintan28@gmail.com>
2024-03-13 23:54:39 +0800

    add ch3-ex3

---
+++ sq-ch3-ex3/src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        System.out.println(context.getBean(Person.class));
+        System.out.println(context.getBean(Parrot.class));
+        System.out.println(context.getBean(Person.class).getParrot());
+    }
+}
