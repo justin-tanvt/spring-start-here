cd04f3fd2ad961a582e092370498a5c0518e08de
justin-tanvt <justintan28@gmail.com>
2024-03-17 20:54:41 +0800

    [AUTO]: checked in from macbook-air

--- sq-ch7-ex1/src/main/java/com/example/Main.java
+++ sq-ch7-ex1/src/main/java/com/example/Main.java
@@ -1,13 +1,13 @@
-package com.example.sqch7ex1;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SqCh7Ex1Application {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqCh7Ex1Application.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
