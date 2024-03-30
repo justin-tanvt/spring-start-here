055606a06dce6f94261ea193fdaba2ee7765782b
justin-tanvt <justintan28@gmail.com>
2024-03-31 01:43:03 +0800

    [AUTO]: checked in from macbook-air

---
+++ provided-code/sq-ch9-ex1/src/main/java/com/example/controllers/LoginController.java
@@ -0,0 +1,45 @@
+package com.example.controllers;
+
+import com.example.model.LoginProcessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+  private final LoginProcessor loginProcessor;
+
+  public LoginController(LoginProcessor loginProcessor) {
+    System.out.println(this + " instantiated!");
+    this.loginProcessor = loginProcessor;
+    System.out.println("loginProcessor set");
+  }
+
+  @GetMapping("/")
+  public String loginGet() {
+    return "login.html";
+  }
+
+  @PostMapping("/")
+  public String loginPost(
+      @RequestParam String username,
+      @RequestParam String password,
+      Model model
+  ) {
+    model.addAttribute("loginProcessorHash", loginProcessor);
+    loginProcessor.setUsername(username);
+    loginProcessor.setPassword(password);
+    boolean loggedIn = loginProcessor.login();
+
+    if (loggedIn) {
+      model.addAttribute("message", "You are now logged in.");
+    } else {
+      model.addAttribute("message", "Login failed!");
+    }
+
+    return "login.html";
+  }
+}
